3d663ef8c9196cf403a3230db5830a80e5c8f0c4
TA2LSM <semihsenol@gmail.com>
2023-09-21 14:48:04 +0300

    commit

--- main.js
+++ main.js
@@ -5,8 +5,19 @@ const path = require("path");
 const { app, BrowserWindow } = electron;
 let mainWindow;
 
+// Bootstrap 4.0.0 css file used in main.html
+
 app.on("ready", () => {
   // console.log("App Started...");
 
   mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "main.html"),
+      protocol: "file",
+      slashes: true, // manage slash (/) for windows and linux based OS >> file://electron/main.html
+    })
+  );
+
+  // ...
 });
